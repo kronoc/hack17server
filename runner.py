515a6139e776c9cdef39d855ce848a64138b940e
conor.keegan <conor.keegan@workday.com>
2017-11-30 16:55:00 +0000

    json format defined

--- runner.py
+++ runner.py
@@ -1,15 +1,19 @@
 from bottle import route, run, template
 
-def fetchLinks(article_name):
-    #fetch from neo4j here
-    url_root = "https://en.wikipedia.org/wiki/"
-    title = "This Is A Test"
-    return [{ "id": 1, "title": title, "url": generateUrl(title) }, { "id": 2, "name": "Test Item 2" }]
-
 @route('/hack17/<article_name>')
 def returnLinks(article_name):
     articles = fetchLinks(article_name)
     return dict(data=articles)
+
+def fetchLinks(article_name):
+    titles = queryGraph(article_name)
+    title = titles[0]
+    return [{ "id": 1, "title": titles[0], "url": generateUrl(titles[0]) }, { "id": 2, "name": titles[1], "url": generateUrl(titles[1]) }]
+
+def queryGraph(article_name):
+    root_title = article_name.replace("_"," ")    
+    titles = ["Perl","Python","Java","C++","Whitespace","OS/2 Warp"]
+    return titles
 
 def generateUrl(title):
     url_root = "https://en.wikipedia.org/wiki/"
