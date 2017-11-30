40bfd8ec7707d4bc204772fc170c00816b4b437d
conor.keegan <conor.keegan@workday.com>
2017-11-30 16:33:07 +0000

    adding urls

--- runner.py
+++ runner.py
@@ -2,11 +2,17 @@ from bottle import route, run, template
 
 def fetchLinks(article_name):
     #fetch from neo4j here
-    return [{ "id": 1, "name": "Test Item 1" }, { "id": 2, "name": "Test Item 2" }]
+    url_root = "https://en.wikipedia.org/wiki/"
+    title = "This Is A Test"
+    return [{ "id": 1, "title": title, "url": generateUrl(title) }, { "id": 2, "name": "Test Item 2" }]
 
 @route('/hack17/<article_name>')
 def returnLinks(article_name):
     articles = fetchLinks(article_name)
     return dict(data=articles)
+
+def generateUrl(title):
+    url_root = "https://en.wikipedia.org/wiki/"
+    return url_root+title.replace(" ","_")
 
 run(host='localhost', port=8080)
