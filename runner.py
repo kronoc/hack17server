159acddf549590eec178e7044c7a7b1dfbfd7b00
conor.keegan <conor.keegan@workday.com>
2017-11-30 16:21:35 +0000

    returning some json

--- runner.py
+++ runner.py
@@ -1,7 +1,12 @@
 from bottle import route, run, template
 
-@route('/guidedwiki/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+def fetchLinks(article_name):
+    #fetch from neo4j here
+    return [{ "id": 1, "name": "Test Item 1" }, { "id": 2, "name": "Test Item 2" }]
+
+@route('/hack17/<article_name>')
+def returnLinks(article_name):
+    articles = fetchLinks(article_name)
+    return dict(data=articles)
 
 run(host='localhost', port=8080)
