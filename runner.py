8f032f638d9b4b3f30eb3b9e297173b738d154d2
conor.keegan <conor.keegan@workday.com>
2017-12-01 11:55:02 +0000

    adding access control header

--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template
+from bottle import route, run, template, request, response
 from neo4j.v1 import GraphDatabase, basic_auth
 from random import *
 
@@ -10,24 +10,27 @@ def get_db():
 @route('/graph/<article>')
 def get_graph(article):
     db = get_db()
-    query = 'MATCH (p:Page {title:"%s"})<-[:Link]-(o:Page),(o) <-[:Link]-(q:Page) With o,count(q) as rel_count RETURN o.title as link_title, rel_count' % article.replace("_"," ")
+    query = 'MATCH (p:Page {title:"%s"})<-[:Link]-(o:Page),(o) <-[:Link]-(q:Page) With o,count(q) as rel_count RETURN o.title as link_title, rel_count Limit 100' % article.replace("_"," ")
 #    query = 'MATCH (p:Page {title:"%s"})<-[:Link]-(o:Page) RETURN o.title as link' % article.replace("_"," ")
     results = db.run(query)
     nodes = []
     rels = []
     i = 0
     for record in results:
-        nodes.append({"title": record["link_title"], "url": generateUrl(record["link_title"]), "rel_count": record["rel_count"],"rev_link": "true"})
-#        nodes.append({"title": record["link"], "url": generateUrl(record["link"]), "rel_count": randint(1, 100), "rev_link": "true"})
+        nodes.append({"title": record["link_title"], "url": generateUrl(record["link_title"]), "rel_count": record["rel_count"],"rev_link": is_backlinking(article, record["link_title"])})
         target = i
         i += 1
+    response.set_header('Access-Control-Allow-Origin', '*')
     return dict(pages=nodes)
 
-def get_links_count(article):
-    db = get_db()
-    query = 'MATCH (p:Page {title:"%s"})<-[:Link]-(o:Page) RETURN count(o)' % article.replace("_"," ")
-    result = db.run(query)
-    return result
+def is_backlinking(parent, child):
+	return "true"
+	#db = get_db()
+	#query = 'MATCH (p:Page {title:"%s"})<-[:Link]-(o:Page) Where o.title = "%s" RETURN true LIMIT 1' % (child.replace("_"," "), parent.replace("_"," "))
+	#if db.run(query):
+	#	return "true"
+	#else:
+	#	return "false"
 
 
 def generateUrl(title):
